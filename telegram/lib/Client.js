51548280d12626c451f8fc42f8194d4a14f521f8
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2019-03-17 17:31:36 +0100

    client class completed

--- telegram/lib/Client.js
+++ telegram/lib/Client.js
@@ -5,23 +5,21 @@ class Client {
     this.axiosInstance = axios.create({ baseURL })
   }
 
-  //WIP
-  async getTournamentTypes () {
+  async getTournamentMatches (tournamentID, toPlay = false) {
     try {
-      const response = await this.axiosInstance.get(`/tournaments/types/`)
+      const response = await this.axiosInstance.get(`/tournaments/${tournamentID}/matches?toPlay=${toPlay}`)
       return response.data
     } catch (error) {
       return new Error(`Cannot get tournament types: ${error.message}`)
     }
   }
 
-  //WIP
   async isTournamentActive (tournamentID) {
     try {
-      await this.axiosInstance.get(`/tournaments/${tournamentID}`)
+      await this.axiosInstance.get(`/tournaments/${tournamentID}/matches`)
       return true
     } catch (error) {
-      if (error.statusCode === 404) {
+      if (error.response.status === 400) {
         return false
       } else {
         return new Error(`Cannot get status for tournament with id ${tournamentID}: ${error.message}`)
@@ -29,49 +27,21 @@ class Client {
     }
   }
 
-  //createTournament creates the tournament and returns the identifier of the tournament
-  async createTournament ({type, teams }) {
+  async createTournament (teamNames) {
     try {
-      const {name} = await this.axiosInstance.post(`/tournaments/`, {
-        type,
-        teams
-      })
-      return name
-    } catch (error) {
-      return new Error(`Cannot create new tournament with id ${tournamentID}: ${error.message}`)
-    }
-  }
-
-  //get tournament results
-  async recordMatchScores (matchId, tournamentId, scoreTeam1, scoreTeam2) {
-    try {
-      const response = await this.axiosInstance.post(`/tournaments/${tournamentId}/matches/${matchId}`, { scoreTeam1, scoreTeam2 })
-      const { data } = response
-
-      return `il tuo torneo: ${JSON.stringify(data)}`
-
-    } catch (error) {
-      return new Error(`Cannot save results ${tournamentId}: ${error.message}`)
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-  async getTournamentMatch ({ tournamentID, matchID }) {
-    try {
-      const response = await this.axiosInstance.get(`/tournaments/${tournamentID}/matches/${matchID}`)
+      const response = await this.axiosInstance.post(`/tournaments`, teamNames)
       return response.data
     } catch (error) {
-      return new Error(`Cannot get games for tournament with id ${tournamentID}: ${error.message}`)
+      return new Error(`Cannot create new tournament.`)
+    }
+  }
+
+  async recordMatchScore (matchId, tournamentId, scoreTeam1, scoreTeam2) {
+    try {
+      const response = await this.axiosInstance.post(`/tournaments/${tournamentId}/matches/${matchId}`, { scoreTeam1, scoreTeam2 })
+      return response.data
+    } catch (error) {
+      return new Error(`Cannot save results.`)
     }
   }
 }
--- telegram/lib/__tests__/Client.test.js
+++ telegram/lib/__tests__/Client.test.js
@@ -1,32 +1,119 @@
+/* eslint-disable no-undef */
 const moxios = require('moxios')
 const Client = require('../Client')
 
-describe('Client', function () {
-  describe('getTournamentTypes', function () {
-    beforeEach(function () {
-      moxios.install()
-    })
+const tournamentMatches = require('./data/matches.json')
+const remainingMatches = require('./data/remainingMatches.json')
+const teamNames = require('./data/teamNames.json')
+const tournament = require('./data/tournament.json')
 
-    afterEach(function () {
-      moxios.uninstall()
-    })
+describe('Client', () => {
+  beforeEach(function () {
+    moxios.install()
+  })
 
-    it('returns tournament types', async function () {
-      const tournamentTypes = [
-        {
-          id: 0,
-          name: 'TYPE_A',
-          description: 'description'
-        }
-      ]
-      moxios.stubRequest('http://local.com/tournaments/types/', {
+  afterEach(function () {
+    moxios.uninstall()
+  })
+  describe('get tournament matches', () => {
+    it('returns tournament matches', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches?toPlay=false', {
         status: 200,
-        response: tournamentTypes
+        response: tournamentMatches
       })
 
       const client = new Client({ baseURL: 'http://local.com' })
-      const response = await client.getTournamentTypes()
-      expect(response).toEqual(tournamentTypes)
+      const response = await client.getTournamentMatches('001')
+      expect(response).toEqual(tournamentMatches)
+    })
+
+    it('should return only the match to play when toPlay is true', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches?toPlay=true', {
+        status: 200,
+        response: remainingMatches
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.getTournamentMatches('001', true)
+      expect(response).toEqual(remainingMatches)
+    })
+
+    it('should return Error if error occurs', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches?toPlay=true', {
+        status: 500
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.getTournamentMatches('001', true)
+      expect(response).toEqual(new Error(`Cannot get tournament types: Request failed with status code 500`))
+    })
+  })
+
+  describe('is tournament active', () => {
+    it('should return true if tournarment is active', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches', {
+        status: 200
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.isTournamentActive('001')
+      expect(response).toEqual(true)
+    })
+
+    it('should return false if tournament is completed', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches', {
+        status: 400
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.isTournamentActive('001')
+      expect(response).toEqual(false)
+    })
+  })
+
+  describe('create tournament', () => {
+    it('should return the tournament id if torunament is created', async () => {
+      moxios.stubRequest('http://local.com/tournaments', {
+        status: 200,
+        response: '001'
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.createTournament(teamNames)
+      expect(response).toEqual('001')
+    })
+
+    it('shouls return error if error occurs', async () => {
+      moxios.stubRequest('http://local.com/tournaments', {
+        status: 500
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.createTournament(teamNames)
+      expect(response).toEqual(new Error(`Cannot create new tournament.`))
+    })
+  })
+
+  describe('record scores', () => {
+    it('should return the updated tournament object if ok', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches/1', {
+        status: 200,
+        response: tournament
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.recordMatchScore('1', '001', 2, 0)
+      expect(response).toEqual(tournament)
+    })
+
+    it('should return error if error occurs', async () => {
+      moxios.stubRequest('http://local.com/tournaments/001/matches/1', {
+        status: 500
+      })
+
+      const client = new Client({ baseURL: 'http://local.com' })
+      const response = await client.recordMatchScore('1', '001', 2, 0)
+      expect(response).toEqual(new Error(`Cannot save results.`))
     })
   })
 })
